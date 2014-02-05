8253f876dc03438deaa694ef8d80f38e3303aa79
Mac Oosthuizen <hello@m-jo.co>
2014-02-05 12:31:25 +0100

    A few title changes

--- Gruntfile.js
+++ Gruntfile.js
@@ -292,7 +292,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'git@github.com:emeasee/mjo-jekyll.git',
           branch: 'gh-pages',
           commit: true,
           push: true
